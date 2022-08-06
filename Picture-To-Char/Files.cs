dca11db1e0dec796c5c7c990a386003bee3299e8
JonasKucinskas <jonaskucinskas2@gmail.com>
2022-08-06 21:32:36 +0300

    m

--- Picture-To-Char/Files.cs
+++ Picture-To-Char/Files.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Picture_To_Char
 {
@@ -12,19 +13,15 @@ namespace Picture_To_Char
     {
         public static string GetCharLine(string filePath)
         {
-            string line;
-
             using (StreamReader read = new StreamReader(filePath))
             {
-                line = read.ReadLine();
+                return read.ReadLine();
             }
-
-            return line; 
         }
 
-        public static void WriteToTXT(List<double> values, string charLine, string path, Bitmap image)
+        public static void WriteToTXT(List<double> values, string charLine, string filePath, Bitmap image)
         {
-            using (StreamWriter write = new StreamWriter(path, false))
+            using (StreamWriter write = new StreamWriter(filePath, false))
             {
                 for (int i = 0; i < image.Height; i++)
                 {
@@ -33,13 +30,29 @@ namespace Picture_To_Char
                         Color color = image.GetPixel(j, i);
                         int grayscale = (int)((0.299 * color.R) + (0.587 * color.G) + (0.114 * color.B));
 
-                        double charNum = (double)values.Count() / charLine.Length;
-                        int numInList = values.IndexOf(grayscale);
-                        write.Write(charLine[(int)(numInList / charNum)]);
+                        double charNum = (double)values.Count() / charLine.Length;//this is probably wrong, but it works for now.
+                        int numInList = values.IndexOf(grayscale);//
+                        char character = charLine[(int)(numInList / charNum)];//
+
+                        //Write character three times to compensate for width difference.
+                        //AA 
+                        //A
+                        //two characters next to each other take less space than two rows.
+
+                        string tripleChar = string.Format("{0}{0}{0}", character);
+
+                        write.Write(tripleChar);
                     }
-                    write.Write("\n");
+
+                    if (i < image.Height)
+                    {
+                        write.Write("\n");
+                    }
                 }
             }
+
+                
+
         }
     }
 }
--- Picture-To-Char/Form1.cs
+++ Picture-To-Char/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -31,18 +32,14 @@ namespace Picture_To_Char
             if (dialog.ShowDialog() == DialogResult.OK)
             {
                 filePath = dialog.FileName;
-                //File.Copy(filePath, Path.Combine(path, fileName));
             }
 
             Bitmap image = new Bitmap(filePath);
             List<double> values = TaskUtils.GetGrayScaleList(image);
-
-            double lightestValue = values[0];
-            double darkestValue = values[values.Count - 1];
-
             string charLine = Files.GetCharLine(charPath);
 
             Files.WriteToTXT(values, charLine, txtPath, image);
+            Process.Start(txtPath);//opens txt file to view.
         }
     }
 }
